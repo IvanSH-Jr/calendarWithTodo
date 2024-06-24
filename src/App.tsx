6dd3877636f0972568f4faae26cb34fe3ac3b1d0
IvanSH-Jr <ivan_parabel@mail.ru>
2024-06-24 14:02:49 +0300

    feats: app can create month & year with all params

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { createDate } from './utils/data/createDate';
+import { createMonth } from './utils/data/createMonth';
 
 const App:React.FC = () => {
-  const test = createDate();
-  console.log(test);
+  const test = createMonth();
+  console.log(test.createMonthDays());
   console.log(new Date(2024, 2, 16))
   return (
     <div className="App">
--- src/utils/data/createMonth.ts
+++ src/utils/data/createMonth.ts
@@ -1,5 +1,5 @@
 import { createDate } from './createDate';
-
+import { getDaysByMonth } from './getDaysByMonth';
 interface CreateMonthParams {
   date?: Date;
   locale?: string;
@@ -10,4 +10,26 @@ export const createMonth = (params?:CreateMonthParams) => {
   const locale = params?.locale ?? 'default';
 
   const myDate = createDate({ date, locale });
+  const { month, monthIndex, monthNumber, year } = myDate;
+
+  const getDay = (dayNumber: number) => 
+    createDate({ date: new Date(year, monthIndex, dayNumber), locale });
+
+  const createMonthDays = () => {
+    const days = [];
+    for (let i = 0; i <= getDaysByMonth(monthIndex, year) - 1; i++) {
+      days[i] = getDay(i + 1);
+    }
+
+    return days;
+  };
+
+  return {
+    getDay,
+    month,
+    monthIndex,
+    monthNumber,
+    year,
+    createMonthDays,
+  };
 };
