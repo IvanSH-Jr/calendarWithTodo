4e5070a1d3059c90252e3d97a3279af2cbf48c07
IvanSH-Jr <ivan_parabel@mail.ru>
2024-06-28 17:07:50 +0300

    init: some styles for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Calendar } from './components/Calendar/Calendar';
 import { formatDate } from './utils/data';
+import './static/css/global.css';
 
 const App:React.FC = () => {
   const [selectedDate, selectDate] = React.useState(new Date());
   return (
-    <div className="App">
+    <div className='app__container'>
       <div>{formatDate(selectedDate, 'DD MM YYY')}</div>
      <Calendar selectDate={selectDate} selectedDate={selectedDate} locale='ru'/>
     </div>
